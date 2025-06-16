7bc7a1ae2915e0c3e9f117e1208230eeb8027c28
m e <duchatelle@gmail.com>
2025-06-16 09:50:07 +0200

    Test Case 1: Register User

--- cypress/e2e/us1.cy.ts
+++ cypress/e2e/us1.cy.ts
@@ -15,6 +15,28 @@ describe('us1 : acces page acceuil spec', () => {
         cy.get('.features_items > :nth-child(3) > .product-image-wrapper')
             .should('be.visible')
 
+
+
+    })
+    it('tc-us01 : register user', () => {
+        cy.visit('https://automationexercise.com/')
+        //home page visible
+        cy.get('body')
+            .should('be.visible')
+        //clic signup button
+        cy.get('.shop-menu > .nav > :nth-child(4) > a').click()
+
+        //verify that 'New User Signup!' is visible
+        cy.contains('New User Signup!').should('be.visible');
+
+        //enter name 'user-test' and 'user-test@mail.com'
+        cy.get('[data-qa="signup-name"]').type('user-test');
+        cy.get('[data-qa="signup-email"]').type('user-test@mail.com');
+
+        //click signup button
+        cy.get('[data-qa="signup-button"]').click()
+
+        /*==== End Cypress Studio ====*/
     })
 })
 
